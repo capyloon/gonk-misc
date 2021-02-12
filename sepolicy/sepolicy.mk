26f51429ec0a5fb1e256d67b00335660e183d156
Fabrice Desré <fabrice@desre.org>
2021-02-12 12:34:22 -0800

    Bug 116969 - remove HW specific sepolicy from gonk-misc. r=me

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -5,8 +5,4 @@ ifeq ($(B2G_SEPOLICY_ENABLED),true)
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += gonk-misc/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += gonk-misc/sepolicy/private
 BOARD_SEPOLICY_DIRS += gonk-misc/sepolicy/vendor
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-BOARD_SEPOLICY_DIRS += gonk-misc/sepolicy/qualcomm/common
-BOARD_SEPOLICY_DIRS += gonk-misc/sepolicy/qualcomm/vendor
-endif
 endif
